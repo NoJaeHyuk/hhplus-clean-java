066de4a5a49dc9a79723d1a4d9bb7a7870beaf07
NOJaeHyuk <jaehyuk7722@naver.com>
2024-03-26 13:49:13 +0900

    first commit

---
+++ src/main/java/com/example/hhpluscleanjava/HhplusCleanJavaApplication.java
@@ -0,0 +1,13 @@
+package com.example.hhpluscleanjava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HhplusCleanJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HhplusCleanJavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hhpluscleanjava/HhplusCleanJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hhpluscleanjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HhplusCleanJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
